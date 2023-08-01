c5b2e01633c32b434370d041ca6fd2adb602c8ee
Wishrito <110767037+Wishrito@users.noreply.github.com>
2023-08-01 11:26:45 +0200

    added tests to .gitignore

--- beta/tests.py
+++
@@ -1,9 +0,0 @@
-import requests, pprint, os, discord
-from discord.ext import commands
-from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageOps
-from datetime import datetime
-TOKEN = "My Bot Token"
-headers = {"Authorization": f"Bot {TOKEN}"}
-userid = 911467405115535411
-req = requests.get(f"https://discord.com/api/v9/users/{userid}", headers=headers)
-pprint.pprint(req.json())
