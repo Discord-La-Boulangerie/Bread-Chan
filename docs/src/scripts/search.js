3ce0523bb59c9a80020816a2800f47a84080700c
Wishrito <110767037+Wishrito@users.noreply.github.com>
2023-10-27 17:37:38 +0200

    cancel

--- docs/src/scripts/search.js
+++ docs/src/scripts/search.js
@@ -1,9 +1,6 @@
 function searchPage() {
     var input = document.getElementById("search-input").value.toLowerCase();
     var sections = document.querySelectorAll("section");
-    var noResultsMessages = document.querySelectorAll(".no-results-message");
-
-    var foundResults = false;
 
     for (var i = 0; i < sections.length; i++) {
         var section = sections[i];
@@ -11,17 +8,8 @@ function searchPage() {
 
         if (content.includes(input)) {
             section.style.display = "block";
-            foundResults = true;
         } else {
             section.style.display = "none";
-        }
-    }
-
-    for (var i = 0; i < noResultsMessages.length; i++) {
-        if (foundResults) {
-            noResultsMessages[i].style.display = "none";
-        } else {
-            noResultsMessages[i].style.display = "block";
         }
     }
 }
