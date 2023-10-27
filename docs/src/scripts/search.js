88d1643fcd4ddd8bec5f1db9f58c60b57e071959
Wishrito <110767037+Wishrito@users.noreply.github.com>
2023-10-27 16:30:42 +0200

    e

--- docs/src/scripts/search.js
+++ docs/src/scripts/search.js
@@ -1,6 +1,9 @@
 function searchPage() {
     var input = document.getElementById("search-input").value.toLowerCase();
     var sections = document.querySelectorAll("section");
+    var noResultsMessage = document.getElementById("no-results-message");
+
+    var foundResults = false; // Pour suivre si des résultats ont été trouvés
 
     for (var i = 0; i < sections.length; i++) {
         var section = sections[i];
@@ -8,8 +11,17 @@ function searchPage() {
 
         if (content.includes(input)) {
             section.style.display = "block";
+            foundResults = true; // Au moins un résultat a été trouvé
         } else {
             section.style.display = "none";
         }
+    }
+
+    if (!foundResults) {
+        // Aucun résultat trouvé, afficher un message d'erreur
+        noResultsMessage.style.display = "block";
+    } else {
+        // Des résultats ont été trouvés, masquer le message d'erreur
+        noResultsMessage.style.display = "none";
     }
 }
