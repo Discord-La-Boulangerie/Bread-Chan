de2218a9f82b89a6628e5c39daa28f1115ed2250
Wishrito <110767037+Wishrito@users.noreply.github.com>
2023-12-14 16:45:02 +0100

    updates

--- docs/src/scripts/search.js
+++ docs/src/scripts/search.js
@@ -1,15 +1,17 @@
-function searchPage() {
-    var input = document.getElementById("search-input").value.toLowerCase();
-    var sections = document.querySelectorAll("section");
+document.getElementById("searchInput").addEventListener("keyup", function () {
+    let input, filter, sections;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    sections = document.querySelectorAll("section");
 
     for (var i = 0; i < sections.length; i++) {
         var section = sections[i];
-        var content = section.textContent.toLowerCase();
+        var content = section.textContent.toUpperCase(); // Utilisez toUpperCase ici pour faire correspondre la casse
 
-        if (content.includes(input)) {
+        if (content.includes(filter)) { // Utilisez le filtre plutôt que l'input
             section.style.display = "block";
         } else {
             section.style.display = "none";
         }
     }
-}
+});
